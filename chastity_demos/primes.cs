0d62c35e0459111d88f5e3a5e3c207d58cc30001
Chastity White Rose <chastitywhiterose@gmail.com>
2023-07-05 22:41:32 -0500

    prime number finding program

--- chastity_demos/primes.cs
+++ chastity_demos/primes.cs
@@ -1,1 +1,27 @@
-ff
+// mcs primes.cs && mono primes.exe
+using System;
+class chastity
+{
+ static void Main(string[] args)
+ {
+  int x,y;
+  int length=1000000;
+  byte[] a=new byte[length];
+  //printf("2\n");
+  Console.WriteLine(2);
+  x=3;
+  while(x<length)
+  {
+   Console.WriteLine(x);
+   y=x;
+   while(y<length)
+   {
+    a[y]=1;
+    y+=x;
+   }
+   while(x<length && a[x]>0){x+=2;}
+  }
+ }
+}
+
+
